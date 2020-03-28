2212060b20bb5182da068854233ae36e0fc20e84
Jan Grzegorowski <grzegorowski@gmail.com>
2020-03-28 20:35:55 +0100

    Setup Rollup bundler

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+const enum EnumTest {
+  VALUE1 = 1,
+  VALUE2 = 2,
+}
+
+const test = (): string => {
+  const abc = [EnumTest.VALUE1, EnumTest.VALUE2];
+  if (abc[0] === EnumTest.VALUE1) {
+    return "test1";
+  }
+  return "test2";
+};
+
+console.log(test());
